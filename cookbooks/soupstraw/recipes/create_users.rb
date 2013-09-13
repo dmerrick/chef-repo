63a24170171ffce759326694dd71f9a61e3b2a48
Dana Merrick <dmerrick@brightcove.com>
2013-09-12 17:34:59 -0700

    Fixing an error if the soupstraw attribute is undefined.

--- cookbooks/soupstraw/recipes/create_users.rb
+++ cookbooks/soupstraw/recipes/create_users.rb
@@ -11,5 +11,5 @@ include_recipe "users::sysadmins"
 # create the deploy user if necessary
 users_manage "deploy" do
   group_id 2301
-  only_if { node[:soupstraw][:deploy_user] }
+  only_if { node[:soupstraw] && node[:soupstraw][:deploy_user] }
 end
