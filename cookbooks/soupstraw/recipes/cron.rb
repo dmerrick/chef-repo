0db59ef39120cb34f4dd2243cc115b4fec00375f
Dana Merrick <dmerrick@brightcove.com>
2014-03-16 20:06:37 -0700

    Making dogwrap only send errors.

--- cookbooks/soupstraw/recipes/cron.rb
+++ cookbooks/soupstraw/recipes/cron.rb
@@ -11,7 +11,7 @@ cron_command += " && RACK_ENV=#{node.chef_environment} #{rake_task}"
 
 # if we're using datadog, send them an event when we run this cron job
 if node['datadog']['api_key']
-  cron_command = "/usr/local/bin/dogwrap -n cron_btc_snapshot -k #{node['datadog']['api_key']} -m all \
+  cron_command = "/usr/local/bin/dogwrap -n cron_btc_snapshot -k #{node['datadog']['api_key']} -m errors \
   '#{cron_command} > /dev/null'"
 end
 
